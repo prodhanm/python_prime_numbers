7ce31eadc0073cb28060f0ef83869f20951f6c70
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-27 10:44:25 -0400

    created a number list.

--- prime_nums.py
+++ prime_nums.py
@@ -1,7 +1,13 @@
 import sys
 
+n = 250
+
+def nums():
+    nums_list = [num for num in range(1,n)]
+    return nums_list
+
 def main():
-    pass
+    print(nums())
 
 if __name__ == "__main__":
     main()
