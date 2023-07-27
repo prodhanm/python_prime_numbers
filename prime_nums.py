5bd04be1601c9080d48ae7c9ba3910e0fc7fdd83
prodhanm <mustafizur.prodhan@gmail.com>
2023-07-27 12:22:33 -0400

    tech debt for refactoring code.

--- prime_nums.py
+++ prime_nums.py
@@ -1,13 +1,17 @@
 import sys
 
-n = 250
-
-def nums():
-    nums_list = [num for num in range(1,n)]
-    return nums_list
+def prime_nums(nums):
+    prime = True
+    for num in range(2,21):
+        if nums%nums == 0:
+            prime = False
+    if prime:
+        print(f"{nums} is prime.")
+    else:
+        print(f"{nums} is not prime")
 
 def main():
-    print(nums())
+    return prime_nums(3)
 
 if __name__ == "__main__":
     main()
